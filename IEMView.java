ceccda1a43a11d9d9e256e000f13a267b89e7a4d
Seb <sebastian.arifin@protonmail.com>
2025-11-27 23:32:53 +0700

    init

---
+++ IEMView.java
@@ -0,0 +1,20 @@
+package A2;
+
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class IEMView {
+    
+}
